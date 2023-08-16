f2866414ef672c998cc1bde1fbad844fe910b00f
szacikdev <142408841+szacikdev@users.noreply.github.com>
2023-08-16 14:27:23 +0200

    Add items for smoothie to pick

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,5 +8,12 @@ streamlit.text('🥗 Strawberry smoothie')
 streamlit.text('🐔 Hard-boiled eggs')
 streamlit.text('🥑🍞 Avocado toast')   
 
+
+streamlit.title('Build your own smoothie')
+# Let's put a pick list here so they can pick the fruit they want to include 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+# Display the table on the page.
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
