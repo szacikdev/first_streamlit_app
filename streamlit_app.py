a8df57f7e213b37148b3eb85745c67ae2f543694
szacikdev <142408841+szacikdev@users.noreply.github.com>
2023-08-16 14:20:54 +0200

    Adding more text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('New Healthy Dinner')
+
+streamlit.header('Breakfast menu')
+streamlit.text('Blueberry oatmeal')
+streamlit.text('Strawberry smoothie')
+streamlit.text('Hard-boiled eggs')
